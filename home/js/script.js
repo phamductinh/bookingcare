27bc67465fb98d54c1da413f0067723ca8b04a0a
phamductinh18 <sundz.1808@gmail.com>
2022-12-25 10:54:34 +0700

    specialties slider

--- home/js/script.js
+++ home/js/script.js
@@ -21,28 +21,17 @@ document.getElementById("prev").onclick = function () {
 	let lists = document.querySelectorAll(".slide-item");
 	document.getElementById("slide").prepend(lists[lists.length - 1]);
 };
-
 var slideIndex = 1;
 showSlides(slideIndex);
-
-// Start autoplaying automatically
 var autoplayInterval = setInterval(function () {
-	// Get element via id and click next
 	document.getElementById("next").onclick();
-}, 2500); // Do this every 1 second, increase this!
-
-// Stop function added to button
+}, 2500);
 function stopAutoplay() {
-	// Stop the autoplay
 	clearInterval(autoplayInterval);
 }
-
-// Next/previous controls
 function plusSlides(n) {
 	showSlides((slideIndex += n));
 }
-
-// Thumbnail image controls
 function currentSlide(n) {
 	showSlides((slideIndex = n));
 }
@@ -63,7 +52,6 @@ function showSlides(n) {
 	slides[slideIndex - 1].style.display = "block";
 }
 
-
 //telemedicine slider
 document.getElementById("telem-next").onclick = function () {
 	let lists = document.querySelectorAll(".telem-slide-item");
@@ -72,4 +60,14 @@ document.getElementById("telem-next").onclick = function () {
 document.getElementById("telem-prev").onclick = function () {
 	let lists = document.querySelectorAll(".telem-slide-item");
 	document.getElementById("telem-slide").prepend(lists[lists.length - 1]);
+};
+
+//specialty slider
+document.getElementById("spec-next").onclick = function () {
+	let lists = document.querySelectorAll(".spec-slide-item");
+	document.getElementById("spec-slide").appendChild(lists[0]);
+};
+document.getElementById("spec-prev").onclick = function () {
+	let lists = document.querySelectorAll(".spec-slide-item");
+	document.getElementById("spec-slide").prepend(lists[lists.length - 1]);
 };
