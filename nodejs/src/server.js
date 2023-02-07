296dbab8883c8935c11d3c7c0ab12d895257eba3
phamductinh18 <sundz.1808@gmail.com>
2023-02-07 23:25:40 +0700

    setup env

---
+++ nodejs/src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./configs/viewEngine";
+import initWebRoutes from "./routes/web";
+require("dotenv").config();
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+viewEngine(app);
+initWebRoutes(app);
+
+let port = process.env.PORT || 9999;
+app.listen(port, () => {
+	console.log("Nodejs is runing on the port" + port);
+});
