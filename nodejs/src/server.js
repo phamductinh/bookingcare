e8a07f7e1a7b106eb14e9544a49d2ae114d8f4d3
phamductinh18 <sundz.1808@gmail.com>
2023-02-07 23:52:25 +0700

    connect db

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -5,6 +5,7 @@ import initWebRoutes from "./routes/web";
 require("dotenv").config();
 
 let app = express();
+let mysql = require("mysql");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,4 +16,19 @@ initWebRoutes(app);
 let port = process.env.PORT || 9999;
 app.listen(port, () => {
 	console.log("Nodejs is runing on the port" + port);
+});
+
+var connectDB = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "bookingcare",
+});
+
+app.get("/test", function (req, res) {
+	var sql = "SELECT * FROM user";
+	connectDB.query(sql, function (err, results) {
+		if (err) throw err;
+		res.send(results);
+	});
 });
