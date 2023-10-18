108b47433f8e46bfaaaa0c310a8288ff9423c82f
phamductinh <phamductinh.t18@gmail.com>
2023-10-18 14:25:07 +0700

    commit

--- nodejs/src/configs/connectDB.js
+++ nodejs/src/configs/connectDB.js
@@ -2,10 +2,17 @@ let mysql = require("mysql");
 
 let connectDB = mysql.createConnection({
 	host: "localhost",
-	user: "root",
-	password: "",
-	database: "bookingcare",
+	user: "pha64077_phamductinh",
+	password: "HkD~90W+M@N.",
+	database: "pha64077_bookingcare",
 });
+
+// let connectDB = mysql.createConnection({
+// 	host: "localhost",
+// 	user: "root",
+// 	password: "",
+// 	database: "bookingcare",
+// });
 
 connectDB.connect(function (err) {
 	if (err) throw err;
